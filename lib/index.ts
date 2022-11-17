fcd6766639522bb2ae88ee2da03cd38ce515e67a
Alex Lewis <alexvlewis@gmail.com>
2022-11-16 21:49:13 -0800

    Skip failing tests

--- lib/index.ts
+++ lib/index.ts
@@ -71,9 +71,8 @@ export async function generateDerivedHDPubkey(masterPrivKey): Promise<string> {
   _dogecoin_ecc_start()
 
   let pub = _malloc(35)
-  let priv = intArrayFromString(masterPrivKey)
 
-  _generateDerivedHDPubkey(priv, pub)
+  _generateDerivedHDPubkey(masterPrivKey, pub)
 
   _dogecoin_ecc_stop()
 
@@ -82,35 +81,27 @@ export async function generateDerivedHDPubkey(masterPrivKey): Promise<string> {
 
 export async function verifyPrivPubKeypair(privKey, pubKey): Promise<boolean> {
   const libdogecoin = await loadWASM()
-  // const {
-  //   _dogecoin_ecc_start,
-  //   _dogecoin_ecc_stop,
-  //   _verifyPrivPubKeypair,
-  //   UTF8ToString,
-  //   intArrayFromString,
-  //   _malloc,
-  // } = libdogecoin
-
-  // _dogecoin_ecc_start()
-
-  // let pub = intArrayFromString(pubKey)
-  // let priv = intArrayFromString(privKey)
-  // let result = _verifyPrivPubKeypair(priv, pub, false)
-
-  // _dogecoin_ecc_stop()
-
-  const { ccall, _dogecoin_ecc_start, _dogecoin_ecc_stop } = libdogecoin
+  const { _dogecoin_ecc_start, _dogecoin_ecc_stop, _verifyPrivPubKeypair } =
+    libdogecoin
 
   _dogecoin_ecc_start()
 
-  var result = ccall(
-    'verifyPrivPubKeypair', // name of C function
-    'number', // return type
-    ['string', 'string', 'boolean'], // argument types
-    [privKey, pubKey, false] // arguments
-  )
+  let result = _verifyPrivPubKeypair(privKey, pubKey, false)
 
   _dogecoin_ecc_stop()
+
+  // const { ccall, _dogecoin_ecc_start, _dogecoin_ecc_stop } = libdogecoin
+
+  // _dogecoin_ecc_start()
+
+  // var result = ccall(
+  //   'verifyPrivPubKeypair', // name of C function
+  //   'number', // return type
+  //   ['string', 'string', 'boolean'], // argument types
+  //   [privKey, pubKey, false] // arguments
+  // )
+
+  // _dogecoin_ecc_stop()
 
   return result
 }
